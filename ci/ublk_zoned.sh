9b28a0a32507aa5e2b888ce086ef3d71cdc04179
Ming Lei <tom.leiming@gmail.com>
2023-10-15 01:43:01 +0000

    ci: make all ublk devices for running blktests
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>

--- ci/ublk_zoned.sh
+++ ci/ublk_zoned.sh
@@ -1,25 +1,87 @@
 #!/bin/bash
 
+add_ublk_dev()
+{
+	local dev_id=$1
+	shift 1
+	local add_cmd="$@"
+	local dev_name="/dev/ublkb${dev_id}"
+
+	if [ -b $dev_name ]; then
+			echo "error"
+	else
+		eval "$add_cmd" > /dev/null 2>&1
+		sleep 1
+		if [ -b $dev_name ]; then
+			echo $dev_name
+		else
+			echo "error"
+		fi
+	fi
+}
+
+build_ublk_dev_list()
+{
+		local rublk=$1
+		local dev_id=$2
+		local backing_file=$3
+		local dev_list=""
+		local dev_path=""
+
+		dev_path=`add_ublk_dev $dev_id $rublk add null -n $dev_id -q 2 -d 64`
+		if [ "$dev_path" != "error" ]; then
+				dev_list="$dev_path "$dev_list
+				let dev_id++
+		fi
+
+		truncate -s 1g $backing_file
+		dev_path=`add_ublk_dev $dev_id $rublk add loop -n $dev_id -q 2 -d 64 -f $backing_file`
+		if [ "$dev_path" != "error" ]; then
+				dev_list="$dev_path "$dev_list
+				let dev_id++
+		fi
+
+		echo $dev_list
+}
+
+build_ublk_zoned_list()
+{
+		local rublk=$1
+		local dev_id=$2
+		local dev_list=""
+		local dev_path=""
+
+		dev_path=`add_ublk_dev $dev_id $rublk add zoned -n $dev_id --zone-size 4`
+		if [ "$dev_path" != "error" ]; then
+				dev_list="$dev_path "$dev_list
+				echo $dev_list
+		else
+				exit -1
+		fi
+}
+
+
 RUBLK=$1
 BLKTESTS_TOP=$2
+TMPF=`mktemp`
 
 modprobe ublk_drv
+$RUBLK del -a
 
-if `$RUBLK features | grep ZONED > /dev/null 2>&1`; then
-		echo "kernel support ublk-zoned"
+#add_ublk_dev 0 $RUBLK add null -n 0 -q 2 -d 64
+#exit 0
 
-		$RUBLK del -a
-		$RUBLK add zoned --zone-size=4 --size=512 > /dev/null 2>&1
-		sleep 2
-
-		if `$RUBLK list | grep zoned > /dev/null`; then
-				echo "found ublk zoned"
-				echo "TEST_DEVS=(/dev/ublkb0)" > $BLKTESTS_TOP/config
-		else
-				echo "TEST_DEVS=()" > $BLKTESTS_TOP/config
-				echo "not found ublk zoned"
-		fi
+if ZDEVL=`build_ublk_zoned_list $RUBLK 0`; then
+	echo "TEST_DEVS=(${ZDEVL})" > $BLKTESTS_TOP/zoned_config
 else
-		echo "kernel doesn't support ublkzoned"
-		echo "TEST_DEVS=()" > $BLKTESTS_TOP/config
+	rm -f $BLKTESTS_TOP/zoned_config
 fi
+
+DEVL=`build_ublk_dev_list $RUBLK 1 $TMPF`
+echo "TEST_DEVS=(${DEVL})" > $BLKTESTS_TOP/config
+
+unlink $TMPF
+
+set -x
+[ -f $BLKTESTS_TOP/zoned_config ] && cat $BLKTESTS_TOP/zoned_config
+cat $BLKTESTS_TOP/config
