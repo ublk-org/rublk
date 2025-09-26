9e28590ec54637e858f35e24757a5f378f5f9612
Ming Lei <tom.leiming@gmail.com>
2025-09-26 02:29:29 +0000

    optimize: reduce eventfd writes in Notifier with counter
    Add RefCell<u32> counter to batch notifications and minimize
    unnecessary eventfd syscalls. Only write to eventfd when
    counter transitions from 0 to 1, coalescing subsequent
    notifications until event_read() resets the counter.
    
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>

--- src/notifier.rs
+++ src/notifier.rs
@@ -1,19 +1,27 @@
 use libublk::{io::UblkQueue, UblkError};
 use nix::sys::eventfd::{EfdFlags, EventFd};
+use std::cell::RefCell;
 use std::os::fd::AsRawFd;
 
 pub(crate) struct Notifier {
     eventfd: EventFd,
+    counter: RefCell<u32>,
 }
 
 impl Notifier {
     pub fn new() -> Result<Self, std::io::Error> {
         let eventfd = EventFd::from_value_and_flags(0, EfdFlags::EFD_CLOEXEC)?;
-        Ok(Notifier { eventfd })
+        Ok(Notifier {
+            eventfd,
+            counter: RefCell::new(0),
+        })
     }
 
     pub fn notify(&self) -> anyhow::Result<()> {
-        nix::unistd::write(&self.eventfd, &1u64.to_le_bytes())?;
+        let old_value = self.counter.replace_with(|&mut old| old + 1);
+        if old_value == 0 {
+            nix::unistd::write(&self.eventfd, &1u64.to_le_bytes())?;
+        }
         Ok(())
     }
 
@@ -24,6 +32,8 @@ impl Notifier {
             io_uring::opcode::Read::new(io_uring::types::Fd(eventfd), buf.as_mut_ptr(), 8).build();
         log::debug!("before eventfd reading");
         let res = q.ublk_submit_sqe(sqe).await;
+
+        self.counter.replace(0);
 
         if res < 8 {
             Err(UblkError::OtherError(-libc::EIO))
